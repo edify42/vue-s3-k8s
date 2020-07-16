ab6b21a64000ad5049c66956dbd044ec28fba61f
ed kim <ekim8015@gmail.com>
2020-07-16 15:49:13 +1000

    this seems to work
    Signed-off-by: ed kim <ekim8015@gmail.com>

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,9 @@
+let endpoint = 'https://k8s-reconciler-poc.lendi-paas-dev.net/lambda'
+
+if (window.location.hostname === 'localhost') {
+  endpoint = 'http://localhost:8080/lambda'
+}
+
 export default {
-  AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'https://k8s-reconciler-poc.lendi-paas-dev.net/lambda'
+  AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: endpoint
 }
--- frontend/src/lambda.js
+++ frontend/src/lambda.js
@@ -4,6 +4,7 @@ import config from './config'
 export default {
   getSignedURL (file) {
     let endpoint = config.AWS_LAMBDA_GETSIGNEDURL_ENDPOINT
+    console.log(window.location.hostname)
     let payload = {
       filePath: file.name,
       contentType: file.type
